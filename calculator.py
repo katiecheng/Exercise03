0301a073199b35f7a28efb0831f5d8415fa3fae5
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 16:14:05 -0700

    working code, all functions as specified (especially /)

--- calculator.py
+++ calculator.py
@@ -2,11 +2,35 @@ import arithmetic
 
 def main():
     calculator_quit = False
+
     while calculator_quit == False:
+
         user_input = raw_input("> ")
         input_array = user_input.split(" ")
-        if input_array[0] == "+":
-            print arithmetic.add(int(input_array[1]),int(input_array[2]))
+
+        sym_list_2var = ["+","-","*","/","pow","mod"]
+        func_list_2var = ["add", "subtract","multiply","divide","power","mod"]
+        sym_list_1var = ["square","cube"]
+
+        first_sym = input_array[0]
+
+        if first_sym in sym_list_2var:
+            sym_index = sym_list_2var.index(first_sym)
+            func = func_list_2var[sym_index]
+            func_call = getattr(arithmetic, func)
+            value = func_call(int(input_array[1]), int(input_array[2]))
+
+            if first_sym == "/":
+                print "%.6f" %value
+            else:
+                print value
+
+        elif first_sym in sym_list_1var:
+            sym_index = sym_list_1var.index(first_sym)
+            func = sym_list_1var[sym_index]
+            func_call = getattr(arithmetic, func)
+            print func_call(int(input_array[1]))
+
         elif user_input == 'q':
             calculator_quit = True
         else:
