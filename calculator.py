c8802f9a6dc88782e6b2e417f1027ccd104e2e60
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 17:14:01 -0700

    fixed divide by 0 Traceback Error

--- calculator.py
+++ calculator.py
@@ -9,10 +9,10 @@ def symbol_to_function_call(symbol):
     func_call = getattr(arithmetic, function)
     return func_call
 
-def main():
+def calculate():
     calculator_quit = False
 
-    while calculator_quit == False:
+    while not calculator_quit:
 
         user_input = raw_input("> ")
         input_array = user_input.split(" ")
@@ -23,19 +23,26 @@ def main():
             func_call = symbol_to_function_call(first_sym)
             print func_call(int(input_array[1]))
 
+        elif first_sym == "/":
+            if input_array[2] == "0":
+                print "Cannot divide by 0"
+            else:
+                func_call = symbol_to_function_call(first_sym)
+                value = func_call(int(input_array[1]), int(input_array[2]))
+                print "%.6f" %value
+
         elif first_sym in sym_list:
             func_call = symbol_to_function_call(first_sym)
             value = func_call(int(input_array[1]), int(input_array[2]))
-
-            if first_sym == "/":
-                print "%.6f" %value
-            else:
-                print value
+            print value
 
         elif user_input == 'q':
             calculator_quit = True
         else:
-            print "unrecognized input"
+            print "Unrecognized input"
+
+def main():
+    calculate()
 
 if __name__ == "__main__":
     main()
